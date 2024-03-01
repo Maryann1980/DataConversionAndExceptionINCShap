139e59e14b1c138b4155c50a01341f26b274623e
USER <USER@DESKTOP-GSI7M8Q>
2024-03-01 16:25:08 +0100

    Add project files.

---
+++ DataConversionAndExceptionINCShap/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DataConversionAndExceptionINCShap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Conversion and Exception in C#");
+            Console.WriteLine();
+
+            Input: while (true)
+            {
+                try
+                {
+                    Rectangle myrec = new Rectangle();
+                    Console.WriteLine("Enter the length : ");
+                    myrec.SetLength(Convert.ToDouble(Console.ReadLine()));
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter the width : ");
+                    myrec.SetWidth(Convert.ToDouble(Console.ReadLine()));
+                    Console.WriteLine("The Area of the rectangle is : {0}", myrec.Area());
+
+                    int MyArea = (int)myrec.Area(); //converted from double to int
+                    Console.WriteLine("The area after converting to int is : {0}", MyArea);
+
+                    Console.WriteLine("Boxing Output");
+                    object boxedOutput = MyArea;
+                    Console.WriteLine("This is boxing return : {0}", boxedOutput);
+
+                    int unboxedOutput = (int)boxedOutput;
+                    Console.WriteLine("The unboxed value is : {0}", unboxedOutput);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ DataConversionAndExceptionINCShap/Rectangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConversionAndExceptionINCShap
+{
+    internal class Rectangle
+    {
+        private double _width;
+        private double _length;
+
+        public Rectangle()
+        {
+            
+
+
+
+        }
+        public Rectangle(double width, double length)
+        {
+            SetLength(Length);
+            SetWidth(Width);
+        }
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+        }
+
+        public void SetLength(double Value)
+        {
+            if (Value > 0)
+            {
+                _length = Value;
+            }
+            else
+            {
+                throw new ArgumentException("The length must not be less than or equal to 0");
+            }
+        }
+        public void SetWidth(double Value)
+        {
+            if (Value > 0)
+            {
+                _width = Value;
+            }
+            else
+            {
+                throw new ArgumentException("The width must not be less than or equal to 0");
+            }
+
+        }
+
+        public double Area()
+        {
+            return Length * Width;
+        }
+    }
+}
